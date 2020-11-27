c2a6abbdee3fc70b4e5066fbf62e4b79fe0906a3
saed-5x <saed.x.stud@gmail.com>
2020-11-28 01:15:36 +0400

    Lab_1 is ready

---
+++ Lab_4/Lab_4/Parking.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+
+    /// <summary>
+    /// Параметризованный класс для хранения набора объектов от интерфейса ITransport
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Parking<T> where T : class, ITransport
+    {
+        /// <summary>
+        /// Список объектов, которые храним
+        /// </summary>
+        private readonly List<T> places;
+        /// <summary>
+        /// Максимальное количество мест на парковке
+        /// </summary>
+        private readonly int _maxCount;
+        /// <summary>
+        /// Ширина окна отрисовки
+        /// </summary>
+        private readonly int pictureWidth;
+        /// <summary>
+        /// Высота окна отрисовки
+        /// </summary>
+        private readonly int pictureHeight;
+        /// <summary>
+        /// Размер парковочного места (ширина)
+        /// </summary>
+        private readonly int _placeSizeWidth = 210;
+        /// <summary>
+        /// Размер парковочного места (высота)
+        /// </summary>
+        private readonly int _placeSizeHeight = 80;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="picWidth">Рамзер парковки - ширина</param>
+        /// <param name="picHeight">Рамзер парковки - высота</param>
+        public Parking(int picWidth, int picHeight)
+        {
+            int width = picWidth / _placeSizeWidth;
+            int height = picHeight / _placeSizeHeight;
+            _maxCount = width * height;
+            pictureWidth = picWidth;
+            pictureHeight = picHeight;
+            places = new List<T>();
+        }
+
+
+        /// <summary>
+        /// Перегрузка оператора сложения
+        /// Логика действия: на парковку добавляется автомобиль
+        /// </summary>
+        /// <param name="p">Парковка</param>
+        /// <param name="car">Добавляемый автомобиль</param>
+        /// <returns></returns>
+        /// 
+
+        public static bool operator +(Parking<T> p, T Monorail)
+        {
+            int size = p.PictureHeight / _placeSizeHeight;
+
+            for (int i = 0; i <= p.places.Length; i++)
+            {
+                if (p._places[i] == null)
+                {
+
+                    Monorail.SetPosition((i / size) * placeSizeWidth + 400, (i % size) * _placeSizeHeight + 15, p.PictureWidth, p.PictureHeight);
+                    p.places[i] = Monorail;
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+
+
+
+        /// <summary>
+        /// Перегрузка оператора вычитания
+        /// Логика действия: с парковки забираем автомобиль
+        /// </summary>
+        /// <param name="p">Парковка</param>
+        /// <param name="index">Индекс места, с которого пытаемся извлечь объект</param>
+        /// <returns></returns>
+        public static T operator -(Parking<T> p, int index)
+        {
+            if (index < -1 || index > p.places.Count)
+            {
+                return null;
+            }
+            T monorail = p.places[index];
+            p.places.RemoveAt(index);
+            return monorail;
+        }
+
+
+
+        /// <summary>
+        /// Метод отрисовки парковки
+        /// </summary>
+        /// <param name="g"></param>
+        public void Draw(Graphics g)
+        {
+            DrawMarking(g);
+            for (int i = 0; i < places.Count; ++i)
+            {
+                
+            places[i].SetPosition(5 + i / 5 * _placeSizeWidth + 5, i % 5 * _placeSizeHeight + 15, pictureWidth, pictureHeight);
+                places[i].DrawMonorail(g);
+            }
+        }
+        /// <summary>
+        /// Метод отрисовки разметки парковочных мест
+        /// </summary>
+        /// <param name="g"></param>
+        private void DrawMarking(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black, 3);
+            for (int i = 0; i < pictureWidth / _placeSizeWidth; i++)
+            {
+                for (int j = 0; j < pictureHeight / _placeSizeHeight + 1; ++j)
+                {//линия рамзетки места
+                    g.DrawLine(pen, i * _placeSizeWidth, j * _placeSizeHeight, i * _placeSizeWidth + _placeSizeWidth / 2, j * _placeSizeHeight);
+                }
+                g.DrawLine(pen, i * _placeSizeWidth, 0, i * _placeSizeWidth, (pictureHeight / _placeSizeHeight) * _placeSizeHeight);
+            }
+        }
+    }
+}
+
+
