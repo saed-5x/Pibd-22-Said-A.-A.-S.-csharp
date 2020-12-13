bee9a0d427d67d3168e431ba24ccb94b24145ddf
saed-5x <saed.x.stud@gmail.com>
2020-12-13 19:13:46 +0400

    Fix pull request

---
+++ Lab_1/Lab_1/FormMonorail.cs
@@ -0,0 +1,63 @@
+﻿using lab_1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsMonorail
+{
+    public partial class FormMonorail : Form
+    {
+        private ITransport monorail;
+        public FormMonorail()
+        {
+            InitializeComponent();
+        }
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            monorail.DrawMonorail(gr);
+            pictureBox1.Image = bmp;
+        }
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    monorail.MoveMonorail(Direction.Up);
+                    break;
+                case "buttonDown":
+                    monorail.MoveMonorail(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    monorail.MoveMonorail(Direction.Left);
+                    break;
+                case "buttonRight":
+                    monorail.MoveMonorail(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+        private void buttonCreateLocomotive_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            monorail = new Locomotive(rnd.Next(150, 300), rnd.Next(1000, 2000), Color.Gold, Color.DarkGreen, true, true,true);
+            monorail.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBox1.Width, pictureBox1.Height);
+            Draw();
+        }
+        private void buttonCreateMonorail_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            monorail = new Monorail(rnd.Next(100, 300), rnd.Next(1000, 2000),Color.Gold, Color.DarkGreen,  true, true,false);
+            monorail.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBox1.Width, pictureBox1.Height);
+            Draw();
+        }
+    }
+}
---
+++ Lab_1/Lab_1/Locomotive.cs
@@ -0,0 +1,133 @@
+﻿using lab_1;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsMonorail
+{
+    public class Locomotive : Vehicle
+    {
+        protected const int MonorailWidth = 200;
+        protected const int MonorailHeight = 150;
+
+        public Locomotive(int maxSpeed, float weight, Color bodycolor, Color sidestrip, bool window, bool doors, bool railway)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            SideStrip = sidestrip;
+            BodyColor = bodycolor;
+            Doors = doors;
+            Window = window;
+            Railway = railway;
+        }
+        public override void MoveMonorail(Direction direction)
+        {
+            float step = MaxSpeed * 200 / Weight;
+            switch (direction)
+            {    
+                case Direction.Right:
+                    if (Xstart + step < PicWidth - MonorailWidth)
+                    {
+                        Xstart += step;
+                    }
+                    break;
+                
+                case Direction.Left:
+                    if (Xstart - step > MonorailWidth+150)
+                    {
+                        Xstart -= step;
+                    }
+                    break;
+                
+                case Direction.Up:
+                    if (Ystart - step > 0)
+                    {
+                        Ystart -= step;
+                    }
+                    break;
+                
+                case Direction.Down:
+                    if (Ystart + step < PicHeight - MonorailHeight)
+                    {
+                        Ystart += step;
+                    }
+                    break;
+            }
+        }
+
+        public override void DrawMonorail(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+
+            float resize = 1.1f;
+
+            Brush Locomotive = new SolidBrush(BodyColor);
+            Brush clip = new SolidBrush(Color.Orange);
+            Brush window = new SolidBrush(Color.White);
+            Brush strip = new SolidBrush(SideStrip);
+            Brush Railway = new SolidBrush(Color.Black);
+            Brush wheel = new SolidBrush(Color.White);
+
+            if (base.Railway)
+            {
+                g.FillRectangle(Railway, Xstart - (400 / resize), Ystart + (90 / resize), 600 / resize, 4 / resize);
+                int j = 0;
+                while (j < 500)
+                {
+                    j = j + 40;
+                    g.FillEllipse(Railway, Xstart - (390 - j / resize), Ystart + (63 / resize), 25 / resize, 25 / resize);
+                    g.FillEllipse(wheel, Xstart - (387 - j / resize), Ystart + (66 / resize), 19 / resize, 19 / resize);
+                    g.FillEllipse(Railway, Xstart - (380 - j / resize), Ystart + (73 / resize), 5 / resize, 5 / resize);
+                }
+                int l = 0;
+                while (l < 460)
+                {
+                    l = l + 80;
+                    g.FillRectangle(Railway, Xstart - (430 - l / resize), Ystart + (70 / resize), 60 / resize, 4 / resize);
+                }
+            }
+
+            g.FillRectangle(Locomotive, Xstart - (30 / resize), Ystart, 80 / resize, 60 / resize);
+            g.FillRectangle(Locomotive, Xstart + (120 / resize), Ystart + (20 / resize), 10 / resize, 50 / resize);
+            g.FillRectangle(Locomotive, Xstart + (100 / resize), Ystart , 20 / resize, 20 / resize);
+            g.FillRectangle(Locomotive, Xstart + (30 / resize), Ystart + (20 / resize), 100 / resize, 40 / resize);
+            g.FillRectangle(clip, Xstart - (55 / resize), Ystart + (5 / resize), 25 / resize, 55 / resize);
+            g.FillRectangle(clip, Xstart - (166 / resize), Ystart + (5 / resize), 25 / resize, 55 / resize);
+            g.FillRectangle(clip, Xstart - (295 / resize), Ystart + (5 / resize), 25 / resize, 55 / resize);
+            g.FillRectangle(Locomotive, Xstart - (150 / resize), Ystart, 105 / resize, 60 / resize);
+            g.FillRectangle(Locomotive, Xstart - (270 / resize), Ystart, 105 / resize, 60 / resize);
+            g.FillRectangle(Locomotive, Xstart - (390 / resize), Ystart, 105 / resize, 60 / resize);
+            g.FillRectangle(strip, Xstart - (30 / resize), Ystart + (35 / resize), 160 / resize, 12 / resize);
+            g.FillRectangle(strip, Xstart - (270 / resize), Ystart + (35 / resize), 105 / resize, 12 / resize);
+            g.FillRectangle(strip, Xstart - (390 / resize), Ystart + (35 / resize), 105 / resize, 12 / resize);
+            g.FillRectangle(strip, Xstart - (150 / resize), Ystart + (35 / resize), 105 / resize, 12 / resize);
+
+            if (Doors)
+            {
+                Brush door1 = new SolidBrush(Color.Green);
+                g.DrawRectangle(pen, Xstart - (115 / resize), Ystart + (10 / resize), 30 / resize, 45 / resize);
+                g.FillRectangle(door1, Xstart - (115 / resize), Ystart + (10 / resize), 30 / resize, 45 / resize);
+                g.DrawRectangle(pen, Xstart - (100 / resize), Ystart + (10 / resize), 15 / resize, 45 / resize);
+                Brush door2 = new SolidBrush(Color.Yellow);
+                g.DrawRectangle(pen, Xstart - (235 / resize), Ystart + (10 / resize), 31 / resize, 45 / resize);
+                g.FillRectangle(door2, Xstart - (235 / resize), Ystart + (10 / resize), 30 / resize, 45 / resize);
+                g.DrawRectangle(pen, Xstart - (220 / resize), Ystart + (10 / resize), 16 / resize, 45 / resize);
+                Brush door3 = new SolidBrush(Color.Fuchsia);
+                g.DrawRectangle(pen, Xstart - (355 / resize), Ystart + (10 / resize), 30 / resize, 45 / resize);
+                g.FillRectangle(door3, Xstart - (355 / resize), Ystart + (10 / resize), 30 / resize, 45 / resize);
+                g.DrawRectangle(pen, Xstart - (340 / resize), Ystart + (10 / resize), 15 / resize, 45 / resize);
+            }
+
+            if (Window)
+            {
+                g.DrawRectangle(pen, Xstart - (20 / resize), Ystart + (10 / resize), 40 / resize, 20 / resize);
+                g.FillRectangle(window, Xstart - (20 / resize), Ystart + (10 / resize), 40 / resize, 20 / resize);
+            }
+
+        }
+    }
+    
+}
