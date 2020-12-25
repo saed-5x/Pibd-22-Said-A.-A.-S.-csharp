27812024e37be7bd0ae90a0e49448fad529e77bc
saed-5x <saed.x.stud@gmail.com>
2020-12-26 03:55:11 +0400

    Lab_5 is ready

---
+++ WindowsFormsMonorail/WindowsFormsMonorail/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_1
+{
+    public delegate void Action<Vehicle>(Vehicle Locomotive);
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormDepot());
+        }
+    }
+}
